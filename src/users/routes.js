7c05c809ffa775322f4ed239419a4302bfcf13bb
Nathan John <codesnj@gmail.com>
2023-09-18 23:46:28 +0100

    Changed update functions

--- src/users/routes.js
+++ src/users/routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express")
 const userRouter = Router()
-const { registerUser, login, findUser, findAll, updateUser, deleteUser } = require("./controllers")
+const { registerUser, login, findUser, findAll, updateUsername, updatePassword, updateEmail, deleteUser } = require("./controllers")
 const { hashPass, comparePass, tokenCheck } = require("../middleware/index")
 
 userRouter.post("/users/register", hashPass, registerUser)
 userRouter.post("/users/login", comparePass, login)
 userRouter.delete("/users/delete", tokenCheck, deleteUser)
-userRouter.put("/users/edit", tokenCheck, updateUser)
+userRouter.put("/users/updateUsername", tokenCheck, updateUsername)
+userRouter.put("/users/updatePassword", tokenCheck, updatePassword)
+userRouter.put("/users/updateEmail", tokenCheck, updateEmail)
 userRouter.get("/users/find", tokenCheck, findUser)
 userRouter.get("/users/findAll", tokenCheck, findAll)
 
